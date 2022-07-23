b0f97061bc7a202b8991bbe2ba69fb15e0487582
msavioti <angelosavioti@gmail.com>
2022-07-22 21:07:10 -0300

    uploaded project

---
+++ Assets/Code/DustCreator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DustCreator : MonoBehaviour
+{
+    [SerializeField]
+    private float _spawnInterval = 1f;
+    
+    [SerializeField]
+    private float _spawnChance = 27f;
+
+    [SerializeField]
+    private Transform[] _spawnPoints;
+
+    [SerializeField]
+    private GameObject _dustPrefab;
+
+    private const float HalfSideSize = 1.8f;
+
+    private void Start()
+    {
+        StartCoroutine(_SpawnRoutine());
+    }
+
+    private void SpawnDust()
+    {
+        if (Random.Range(0f, 100f) > _spawnChance)
+        {
+            return;
+        }
+        
+        var dust = Instantiate(_dustPrefab, _spawnPoints[Random.Range(0, _spawnPoints.Length)], false);
+        dust.transform.localPosition = new Vector2(Random.Range(-HalfSideSize, HalfSideSize), Random.Range(-HalfSideSize, HalfSideSize));
+    }
+
+    private IEnumerator _SpawnRoutine()
+    {
+        SpawnDust();
+        yield return new WaitForSeconds(_spawnInterval);
+        StartCoroutine(_SpawnRoutine());
+    }
+}
---
+++ Assets/Code/VacuumCleaner.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VacuumCleaner : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 5f;
+
+    [SerializeField]
+    private LayerMask _dustMask;
+
+    [SerializeField]
+    private Transform[] _spawnPoints;
+
+    private Transform _currentTarget;
+
+    private const float SideSize = 3.6f;
+
+    private const float MinDistance = 0.001f;
+
+    private void Start()
+    {
+        FindMovementTarget();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, SideSize * Vector2.one);
+    }
+
+    private void Move()
+    {
+        var distance = Vector2.Distance(transform.position, _currentTarget.position);
+
+        if (distance < MinDistance)
+        {
+            LookForDust();
+            return;
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, _currentTarget.position, _speed);
+    }
+
+    private void LookForDust()
+    {
+        var results = new Collider2D[30];
+        var resultCount = Physics2D.OverlapBoxNonAlloc(transform.position, Vector2.one * SideSize, 0f, results, _dustMask);
+
+        if (resultCount > 0)
+        {
+            Clean(results);
+        }
+
+        FindMovementTarget();
+    }
+
+    private void Clean(Collider2D[] results)
+    {
+        foreach (var result in results)
+        {
+            if (result)
+            {
+                result.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void FindMovementTarget()
+    {
+        _currentTarget = _spawnPoints[Random.Range(0, _spawnPoints.Length)];
+    }
+}
